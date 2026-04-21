db081dee08e129373053733b293563fc868da184
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-21 14:29:09 -0300

    feat: add create post request

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -1,7 +1,28 @@
 const Post = require("../models/Post");
 
 const createPost = async (req, res) => {
-  res.json("Deu certo!")
+  try {
+    const { title, description, author, image } = req.body;
+
+    if (!title || !description || !author || !image) {
+      res.status(400).json({ msg: "Por favor, preencha todos os campos." });
+      return;
+    }
+
+    const newPost = {
+      title,
+      description,
+      author,
+      image,
+    };
+
+    const response = await Post.create(newPost);
+
+    res.status(201).json({ response, msg: "Post criado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocorreu um erro!");
+  }
 };
 
 module.exports = {
